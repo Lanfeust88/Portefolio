fafc8a05b50fb139a8ea2ef2ed5d2673342a19b2
Cyril <cyril.plou@gmail.com>
2024-05-08 14:00:32 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,8 @@
 let body = document.getElementById("body");
 let divpop = document.createElement("div");
 divpop.classList.add("divpopup");
-let bullecyril = document.createElement("img");
-bullecyril.src="https://github.com/Lanfeust88/Portefolio/blob/main/images/bullecyril.png" ;
+let bullecyril = new Image(100,100);
+bullecyril.src="images/bullecyril.png" ;
 divpop.appendChild(bullecyril);
 let bgpopup = document.createElement("div");
 bgpopup.classList.add("bgpopup");
